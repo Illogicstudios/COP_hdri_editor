c31df468c2effa610f17d076158e078b16f39516
Patateon <maximebeldjilali@gmail.com>
2025-01-08 16:53:28 +0100

    Generalize to blend node with a blend config

--- scripts/multiadd/multiadd.py
+++ scripts/multiadd/multiadd.py
@@ -3,13 +3,15 @@ import hou
 BLENDTYPE = "blend"
 ADDMODE = 3
 
+CONFIG_NODE_NAME = "config_blend"
+
 def onInputChanged(kwargs):
     """
-    Callback on a multi add subnetwork which will adjust the add
+    Callback on a multi blend subnetwork which will adjust the blend
     connections.
 
-    More specifically a multi add subnetwork is a basic subnetwork
-    where you can add multiples layers in input into a single layer.
+    More specifically a multi blend subnetwork is a basic subnetwork
+    where you can blend multiples layers in input into a single layer.
 
     :param kwargs: Subnetwork context
     """
@@ -43,19 +45,22 @@ def onInputChanged(kwargs):
         if connection.outputIndex() == input_to_connect:
             connection.outputNode().setInput(
                 connection.inputIndex(), None)
-            
-            print("Clear an existing connections")
             break
 
-    # Get every blend node in subnet with mode add
-    add_nodes = []
-    add_nodes_with_free_input = []
-    add_node_connected_to_output  = None
+    # Get every blend node in subnet
+    blend_nodes = []
+    blend_nodes_with_free_input = []
+    blend_node_connected_to_output  = None
+    config_node = None
 
     for child in subnet.children():
-        if (child.type().name() == BLENDTYPE)\
-            and (child.parm("mode").eval() == ADDMODE):
-            add_nodes.append(child)
+        if (child.type().name() == BLENDTYPE):
+            
+            if(child.name() == CONFIG_NODE_NAME):
+                config_node = child
+                continue
+
+            blend_nodes.append(child)
 
             # Get every valid input and output
             child_inputs = [input for input in child.inputs()
@@ -63,98 +68,103 @@ def onInputChanged(kwargs):
             child_outputs = [output for output in child.outputs()
                               if output is not None]
             if len(child_inputs) == 1:
-                add_nodes_with_free_input.append(child)
+                blend_nodes_with_free_input.append(child)
             if len(child_outputs) == 1:
-                add_node_connected_to_output  = child
+                blend_node_connected_to_output  = child
 
     # Check if the input changed was a new connection
     connecting = subnet.input(input_to_connect) is not None
 
     if not connecting:
         
-        # Delete every add node with no connection in input
-        cleanAdd(add_nodes)
+        # Delete every blend node with no connection in input
+        cleanBlend(blend_nodes)
 
         # Rewire every node that have only one connection and
         # if that's connection input is fg
-        for add_node in add_nodes:
-            add_node_inputs = [input for input in add_node.inputs()
+        for blend_node in blend_nodes:
+            blend_node_inputs = [input for input in blend_node.inputs()
                                 if input is not None]
             
-            if len(add_node_inputs) == 1:
-                connection = add_node.inputConnections()[0]
+            if len(blend_node_inputs) == 1:
+                connection = blend_node.inputConnections()[0]
                 # If the only connection of the node is on fg
                 # Then swap it to bg
                 if connection.inputIndex() == 1:
-                    add_node.setInput(0,
+                    blend_node.setInput(0,
                                     connection.inputNode(),
                                     connection.outputIndex())
-                    add_node.setInput(1, None)
+                    blend_node.setInput(1, None)
 
         subnet.layoutChildren()
         return 
 
 
     # Try to connect to a node with a free slot
-    if add_nodes_with_free_input:
-        print("Plug to a free input")
-
+    if blend_nodes_with_free_input:
         for input_index, connectors in enumerate(
-            add_nodes_with_free_input[0].inputConnectors()):
+            blend_nodes_with_free_input[0].inputConnectors()):
             if len(connectors) == 0 and input_index != 2:
-                add_nodes_with_free_input[0].setInput(
+                blend_nodes_with_free_input[0].setInput(
                     input_index, inputs_node, input_to_connect)
                 break
-    # Else add a new blend node with add type
+    # Else add a new blend node
     else:
-        if add_node_connected_to_output  is not None:
+        if blend_node_connected_to_output  is not None:
 
             outputs_node.setInput(0, None)
 
-            new_add = createAdd(subnet)
+            new_blend = createBlend(subnet, config_node)
 
-            new_add.setInput(0, add_node_connected_to_output , 0)
-            new_add.setInput(1, inputs_node, input_to_connect)
-            outputs_node.setInput(0, new_add, 0)
+            new_blend.setInput(0, blend_node_connected_to_output , 0)
+            new_blend.setInput(1, inputs_node, input_to_connect)
+            outputs_node.setInput(0, new_blend, 0)
 
         # If there is no blend node with the output node as output
         # we need output our new blend here
         else:
-            new_add = createAdd(subnet)
-            new_add.setInput(0, inputs_node, 0)
-            subnet.subnetOutputs()[0].setInput(0, new_add, 0)
+            new_blend = createBlend(subnet, config_node)
+            new_blend.setInput(0, inputs_node, 0)
+            subnet.subnetOutputs()[0].setInput(0, new_blend, 0)
 
-            for add in add_nodes:
-                if not len(add.outputs()):
-                    add_node_connected_to_output  = add
+            for blend in blend_nodes:
+                if not len(blend.outputs()):
+                    blend_node_connected_to_output  = blend
 
-            if add_node_connected_to_output  is not None:
-                new_add.setInput(1, add_node_connected_to_output)
+            if blend_node_connected_to_output  is not None:
+                new_blend.setInput(1, blend_node_connected_to_output)
     
     subnet.layoutChildren()
    
 
-def cleanAdd(add_nodes):
+def cleanBlend(blend_nodes):
     """
     Clean recursively every node that does not have inputs.
 
-    :param add_nodes: List of blend nodes with mode add
+    :param blend_nodes: List of blend nodes
     """
-    to_delete = [add for add in add_nodes if not add.inputs()]
+    to_delete = [blend for blend in blend_nodes if not blend.inputs()]
     while to_delete:
-        for add in to_delete:
-            add.destroy()
-            add_nodes.remove(add)
-        to_delete = [add for add in add_nodes if not add.inputs()]
+        for blend in to_delete:
+            if(blend.name() == CONFIG_NODE_NAME):
+                continue
+            blend.destroy()
+            blend_nodes.remove(blend)
+        to_delete = [blend for blend in blend_nodes if not blend.inputs()]
 
 
-def createAdd(subnet):
+def createBlend(subnet, config_node=None):
     """
-    Create a node add with mode add.
+    Create a node blend with mode blend.
 
-    :param subnet: Multi add subnetwork
+    :param subnet: Multi blend subnetwork
     """
 
-    add = subnet.createNode(node_type_name=BLENDTYPE)
-    add.parm("mode").set(ADDMODE)
-    return add
+    if config_node is None:
+        blend = subnet.createNode(node_type_name=BLENDTYPE)
+        blend.parm("mode").set(ADDMODE)
+        return blend
+    else:
+        blend = subnet.copyItems([config_node], channel_reference_originals=True)[0]
+        return blend
+
